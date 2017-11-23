0c7fca09ce5fbbf9619816352ec22a08f2006109
EelkeFeitz <eelkefeitz@live.nl>
2017-11-23 13:47:28 +0100

    Feedback op opdracht 1.2 verwerkt

--- opdracht 1.2.py
+++ opdracht 1.2.py
@@ -13,21 +13,23 @@ numberList:
     a lst with all the numbers in str
 """""
 def get_numbers(s):
-    assert (len(s) != 0) #raises an error if s is an empty str
+    assert len(s) != 0 #raises an error if s is an empty str
     number = 0
     numberList = []
-    for i in range(0, len(s)):
-        if (ord(s[i]) > 47 and ord(s[i]) < 58):
-            number = number * 10 + int(s[i])
+    for e in s:
+        if '0' <= e <= '9':
+            number = number * 10 + int(e)
         else:
             if number != 0:
                 numberList.append(number)
                 number = 0
     if number != 0:
-        numberList.append(number)
+        #numberList.append(number)
+        numberList += [number]
     return numberList
 
 
-s = 'een123zin45 6met-632meerdere+7777getallen'
+s = """ 22een123zin45 6met-63
+2meerdere+7777getallen44"""
 list = get_numbers(s)
 print('Alle getallen:', list)
